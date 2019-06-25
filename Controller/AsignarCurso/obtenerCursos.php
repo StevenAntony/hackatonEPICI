c34ee007eab1154535558ac9fb91a5dd74534020
erickRonaldRodriguezChiscul <39859547+erickRonaldRodriguezChiscul@users.noreply.github.com>
2019-06-25 17:46:19 -0500

    l

--- Controller/AsignarCurso/obtenerCursos.php
+++ Controller/AsignarCurso/obtenerCursos.php
@@ -4,7 +4,7 @@
     $listaCurso = Docente::mostrarCurso($idDocenta);
     foreach ($listaCurso as $key) {
 ?>
-    <option value="<?php echo $key['IdCurso'];?>" keyGroup="<?php echo $key["IdCurGrup"];?>"><?php echo $key["DescripcionCurso"]; ?> (<?php echo $key["CodigoCurso"];?>) - Grupo <?php if($key["IdGrupo"]==1){echo "A";}else{echo "B";}?></option>
+    <option value="<?php echo $key['IdCurso'];?>" horaTeoria="<?php echo $key['HoraT'];?>" horaPractica="<?php echo $key['HoraP'];?>" keyGroup="<?php echo $key["IdCurGrup"];?>"><?php echo $key["DescripcionCurso"]; ?> (<?php echo $key["CodigoCurso"];?>) - Grupo <?php if($key["IdGrupo"]==1){echo "A";}else{echo "B";}?></option>
 <?php
     }
 ?>
