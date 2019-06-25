9b33cb39a745a06b3cd4ba1c19583660e56793d8
HP\casflo <castillofloresstevenantony@gmail.com>
2019-06-25 15:50:55 -0500

    js signar usuario

--- Model/Horario.php
+++ Model/Horario.php
@@ -1,6 +1,15 @@
 <?php
     require 'DataBase.php';
     class Horario{
-        
-    } 
+        public static function HorarioXAmbiente($idAmbiente){
+            $consulta = 'SELECT * FROM `ambiente` a INNER JOIN `horario` h ON a.idAmb = h.idAmb INNER JOIN `curgrup` cg ON cg.IdCurGrup = h.IdCurGrup INNER JOIN `curso` c ON cg.idCurso = c.idCurso INNER JOIN `docente` d ON cg.idDoc = d.idDoc  WHERE a.idAmb ='. $idAmbiente.'';
+            try {
+                $comando = Database::getInstance()->getDb()->prepare($consulta);
+                $comando->execute();
+                return $comando->fetchAll(PDO::FETCH_ASSOC);
+            } catch (PDOException $e) {
+                return false;
+            }
+        }
+    }
 ?>
--- View/Public/Horario/asignarHorario.php
+++ View/Public/Horario/asignarHorario.php
@@ -26,302 +26,141 @@ include_once('../Layout/master.php');
       <div class="col-md-6">
         Docente:
         <div class="input-group"><span class="dDocente" key=""> </span></div>
+      </div>
+    </div>
+    <hr>
+    <div class="row">
+      <div class="col-md-4">
+        <div class="form-group">
+          <span class="small">Curso</span>
+          <select class="form-control">
+            <option>Programacion Avanzada (CL20) - Grupo A</option>
+            <option>Diseño Grafico (DG03) - Grupo B </option>
+          </select>
         </div>
       </div>
-      <hr>
-      <div class="row">
-        <div class="col-md-4">
-          <div class="form-group">
-            <span class="small">Curso</span>
-            <select class="form-control">
-              <option>Programacion Avanzada (CL20) - Grupo A</option>
-              <option>Diseño Grafico (DG03) - Grupo B </option>
-            </select>
-          </div>
-        </div>
-        <div class="col-md-2">
-          <div class="form-group">
-            <span class="small">Tipo</span>
-            <select class="form-control">
-              <option>Teoria</option>
-              <option>Practica</option>
-            </select>
-          </div>
-        </div>
-        <div class="col-md-2">
-          <div class="form-group">
-            <span class="small">Ambiente</span>
-            <select class="form-control">
-              <option>Laboratorio 01</option>
-              <option>Aula 10</option>
-            </select>
-          </div>
+      <div class="col-md-2">
+        <div class="form-group">
+          <span class="small">Tipo</span>
+          <select class="form-control">
+            <option>Teoria</option>
+            <option>Practica</option>
+          </select>
         </div>
       </div>
-      <hr>
-      <div>
-        <span class="small">Horario Semenestre 2019-I del Laboratorio 01</span>
-        <div class="table-responsive">
-          <table class="table table-bordered">
-            <thead style="text-align: center;">
-              <tr>
-                <th>
-
-                </th>
-                <th>
-                  Lunes
-                </th>
-                <th>
-                  Martes
-                </th>
-                <th>
-                  Miercoles
-                </th>
-                <th>
-                  Jueves
-                </th>
-                <th>
-                  Viernes
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td class="py-1">
-                  07:30
-                </td>
-                <td>
-
-                </td>
-                <td>
-
-                </td>
-                <td>
-
-                </td>
-                <td>
-
-                </td>
-                <td>
-
-                </td>
-              </tr>
-              <tr>
-                <td class="py-1">
-                  08:20
-                </td>
-                <td>
-
-                </td>
-                <td>
-
-                </td>
-                <td>
-
-                </td>
-                <td>
-
-                </td>
-                <td>
-
-                </td>
-              </tr>
-              <tr>
-                <td class="py-1">
-                  09:10
-                </td>
-                <td>
-
-                </td>
-                <td>
-
-                </td>
-                <td>
-
-                </td>
-                <td>
-
-                </td>
-                <td>
-
-                </td>
-              </tr>
-              <tr>
-                <td class="py-1">
-                  10:00
-                </td>
-                <td>
-
-                </td>
-                <td>
-
-                </td>
-                <td>
-
-                </td>
-                <td>
-
-                </td>
-                <td>
-
-                </td>
-              </tr>
-              <tr>
-                <td class="py-1">
-                  10:50
-                </td>
-                <td>
-
-                </td>
-                <td>
-
-                </td>
-                <td>
-
-                </td>
-                <td>
-
-                </td>
-                <td>
-
-                </td>
-              </tr>
-              <tr>
-                <td class="py-1">
-                  11:40
-                </td>
-                <td>
-
-                </td>
-                <td>
-
-                </td>
-                <td>
-
-                </td>
-                <td>
-
-                </td>
-                <td>
-
-                </td>
-              </tr>
-              <tr>
-                <td class="py-1">
-                  12:30
-                </td>
-                <td>
-
-                </td>
-                <td>
-
-                </td>
-                <td>
-
-                </td>
-                <td>
-
-                </td>
-                <td>
-
-                </td>
-              </tr>
-              <tr>
-                <td class="py-1">
-                  13:20
-                </td>
-                <td>
-
-                </td>
-                <td>
-
-                </td>
-                <td>
-
-                </td>
-                <td>
-
-                </td>
-                <td>
-
-                </td>
-              </tr>
-              <tr>
-                <td class="py-1">
-                  14:10
-                </td>
-                <td>
-
-                </td>
-                <td>
-
-                </td>
-                <td>
-
-                </td>
-                <td>
-
-                </td>
-                <td>
-
-                </td>
-              </tr>
-              <tr>
-                <td class="py-1">
-                  15:00
-                </td>
-                <td>
-
-                </td>
-                <td>
-
-                </td>
-                <td>
-
-                </td>
-                <td>
-
-                </td>
-                <td>
-
-                </td>
-              </tr>
-              <tr>
-                <td class="py-1">
-                  15:50
-                </td>
-                <td>
-
-                </td>
-                <td>
-
-                </td>
-                <td>
-
-                </td>
-                <td>
-
-                </td>
-                <td>
-
-                </td>
-              </tr>
-            </tbody>
-          </table>
+      <div class="col-md-2">
+        <div class="form-group">
+          <span class="small">Ambiente</span>
+          <select class="form-control">
+            <option>Laboratorio 01</option>
+            <option>Aula 10</option>
+          </select>
         </div>
       </div>
-      <hr>
-      <div class="row">
-        <div class="col-md-2">
-          <button type="button" class="btn btn-primary btn-fw">Guardar Horario</button>
-        </div>
+      <div class="col-md-2">
+        <button type="button" id="MostrarHorario" class="btn btn-primary btn-fw">Mostrar Horario</button>
+      </div>
+    </div>
+    <hr>
+    <div>
+      <span class="small">Horario Semenestre 2019-I del Laboratorio 01</span>
+      <div class="table-responsive">
+        <table class="table table-bordered">
+          <thead style="text-align: center;">
+            <tr>
+              <th>
+
+              </th>
+              <th>
+                Lunes
+              </th>
+              <th>
+                Martes
+              </th>
+              <th>
+                Miercoles
+              </th>
+              <th>
+                Jueves
+              </th>
+              <th>
+                Viernes
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td class="py-1">
+                07:30
+              </td>
+
+            </tr>
+            <tr>
+              <td class="py-1">
+                08:20
+              </td>
+            </tr>
+            <tr>
+              <td class="py-1">
+                09:10
+              </td>
+            </tr>
+            <tr>
+              <td class="py-1">
+                10:00
+              </td>
+            </tr>
+            <tr>
+              <td class="py-1">
+                10:50
+              </td>
+            </tr>
+            <tr>
+              <td class="py-1">
+                11:40
+              </td>
+            </tr>
+            <tr>
+              <td class="py-1">
+                12:30
+              </td>
+            </tr>
+            <tr>
+              <td class="py-1">
+                13:20
+              </td>
+            </tr>
+            <tr>
+              <td class="py-1">
+                14:10
+              </td>
+            </tr>
+            <tr>
+              <td class="py-1">
+                15:00
+              </td>
+            </tr>
+            <tr>
+              <td class="py-1">
+                15:50
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+    <hr>
+    <div class="row">
+      <div class="col-md-2">
+        <button type="button" class="btn btn-primary btn-fw">Guardar Horario</button>
       </div>
     </div>
   </div>
+</div>
 
-  <?php
-  include_once('../Layout/footer.php');
-  ?>
+<?php
+include_once('../Layout/footer.php');
+?>
 
 
-  <script src="http://localhost:2000/EPICI2019/hackatonEPICI/View/Assets/js/asignarCurso.js"></script>
+<script src="http://localhost:2000/EPICI2019/hackatonEPICI/View/Assets/js/asignarCurso.js"></script>
