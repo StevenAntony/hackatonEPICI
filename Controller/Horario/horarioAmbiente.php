ef987214b2fd588ed8eeeaa4211532290348fa32
HP\casflo <castillofloresstevenantony@gmail.com>
2019-06-25 19:46:26 -0500

    Update horarioAmbiente.php

--- Controller/Horario/horarioAmbiente.php
+++ Controller/Horario/horarioAmbiente.php
@@ -1,10 +1,12 @@
 <?php
+
+
 require '../../Model/Horario.php';
-session_start();
-$idUsuario = $_SESSION["idUsuario"];
-$dni = Horario::DniUsuario($idUsuario);
-$variable2 = Horario::mostrarHorario($dni['Usuario']);
+$variable = Horario::HorarioXAmbiente($_POST['ambiente']);
+$variable2 = Horario::HorarioDisponible($_POST['dni']);
+// var_dump($variable);
 $array = [
+  'ambiente' => $variable,
   'disponible' =>  $variable2
 ];
 echo json_encode($array);
