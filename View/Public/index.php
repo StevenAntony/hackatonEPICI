9d2f68d4ccc9964ad2597e0d1c83775069796ca0
erickRonaldRodriguezChiscul <39859547+erickRonaldRodriguezChiscul@users.noreply.github.com>
2019-06-25 19:26:33 -0500

    asd

--- View/Public/index.php
+++ View/Public/index.php
@@ -1,8 +1,8 @@
 <?php
     include("../../Controller/validarSesion.php");
-    include("Layout/footer.php");
+    include("Layout/master.php");
 ?>
 
 <?php
-    include("Layout/master.php");
+    include("Layout/footer.php");
 ?>
