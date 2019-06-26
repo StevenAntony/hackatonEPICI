b7cbc08d6c06aedd45350ac09d9d9fec8fe1c4de
KARINA20081997 <52207289+KARINA20081997@users.noreply.github.com>
2019-06-25 19:21:41 -0500

    t

--- View/Public/Horario/asignarHorario.php
+++ View/Public/Horario/asignarHorario.php
@@ -152,8 +152,8 @@ include_once('../Layout/master.php');
   </div>
 </div>
 
-<div class="card" style="margin-top:25px">
-            <table class="table table-bordered" style="width: -42%;">
+<div class="card" style="margin-top:-42px">
+            <table class="table table-bordered" style="width: 25%;">
               <thead>
                 <tr>
                   <th>Leyenda</th>
